aa27b95adc74f7a97215f4d52be16360a0706fed
daalol <daalol87@gmail.com>
2013-02-03 22:39:16 +0100

    Terminando NHibernatePrueba...

--- PHibernatePrueba/PHibernatePrueba/MainWindow.cs
+++ PHibernatePrueba/PHibernatePrueba/MainWindow.cs
@@ -1,7 +1,7 @@
 using System;
 using Gtk;
 using NHibernate.Cfg;
-using NHibernate.
+using NHibernate.Tool.hbm2ddl;
 using Serpis.Ad;
 
 public partial class MainWindow: Gtk.Window
@@ -9,6 +9,7 @@ public partial class MainWindow: Gtk.Window
 	public MainWindow (): base (Gtk.WindowType.Toplevel)
 	{
 		Build ();
+		//http://darioquintana.com.ar/articles/tutorial-de-nhibernate-primeros-pasos
 		
 		//Clase configuration de NHibernate
 		Configuration configuration = new configuration();
@@ -16,7 +17,20 @@ public partial class MainWindow: Gtk.Window
 		
 		configuration.AddAssembly(typeof(Categoria).Assembly);
 		
-		new SchemaExport(configuration).execute(true,false,false);
+		/*new SchemaExport(configuration).execute(true,false,false);*/
+		
+		//Se encarga crear sesiones en nuestra aplicacion.
+		ISessionFactory sessionFactory = configuration.BuildSessionFactory ();
+		
+		//Abrimos la sesion
+		ISession session = sessionFactory.OpenSession();
+		Categoria categoria = (Categoria)session.Load(typeof(Categoria), 2L);
+		Console.WriteLine("Categoria Id={0} Nombre={1}", categoria.Id, categoria.Nombre);
+		categoria.Nombre = DateTime.Now.ToString ();
+		session.SaveOrUpdate (categoria);
+		session.Close ();
+		
+		sessionFactory.Close ();
 	}
 	
 	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
